8a2b5cb7b08ef308a144027eba40176e4175ea05
azuchi <azuchi@chaintope.com>
2022-10-31 14:57:39 +0900

    initial commit

---
+++ lib/minisketch.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "minisketch/version"
+
+module Minisketch
+  class Error < StandardError
+  end
+  # Your code goes here...
+end
---
+++ spec/minisketch_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Minisketch do
+  it "has a version number" do
+    expect(Minisketch::VERSION).not_to be nil
+  end
+end
